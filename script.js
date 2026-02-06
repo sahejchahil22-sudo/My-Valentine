4f53af818eb62dc7e0daedaa26f7949f7e191353
sahejchahil22-sudo <sahejchahil22@gmail.com>
2026-02-05 23:28:49 -0800

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
+// HEARTS ANIMATION ❤️
 function createHeart() {
-    const heart = document.createElement('div');
-    heart.classList.add('heart');
-    heart.style.left = Math.random() * window.innerWidth + 'px';
-    heart.style.animationDuration = (2 + Math.random() * 3) + 's';
-    heart.style.fontSize = (10 + Math.random() * 30) + 'px';
-    heart.innerText = '❤️';
-    document.getElementById('hearts').appendChild(heart);
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.style.left = Math.random() * window.innerWidth + "px";
+  heart.style.animationDuration = 2 + Math.random() * 3 + "s";
+  heart.style.fontSize = 14 + Math.random() * 30 + "px";
+  heart.innerText = "❤️";
+  document.getElementById("hearts").appendChild(heart);
 
-    setTimeout(() => {
-        heart.remove();
-    }, 5000);
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
+setInterval(createHeart, 300);
+
+
+// QUESTIONS LOGIC 💌
+function answerYes(q) {
+  if (q === 1) {
+    document.getElementById("result1").innerText = "YAYYY 😍 I knew it!!";
+    document.getElementById("q2").style.display = "block";
+  }
+
+  if (q === 2) {
+    document.getElementById("result2").innerText = "STOPPPP 🥹💍❤️";
+    document.getElementById("final").style.display = "block";
+  }
 }
 
-setInterval(createHeart, 300);
+function answerNo(q) {
+  if (q === 1) {
+    document.getElementById("result1").innerText =
+      "Wrong answer 😤 Try again 😌";
+  }
+
+  if (q === 2) {
+    document.getElementById("result2").innerText =
+      "Excuse me?? 😭 Click YES right now 😤❤️";
+  }
+}
+
 
